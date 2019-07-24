0e5ae23b9b99ef3a335f5947fbc764d4229ad9b5
Takashi <Takashi@gmail.com>
2019-07-24 19:33:18 +0900

    propertyモデルにバリデーションを追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
   has_many :sts
   accepts_nested_attributes_for :sts,allow_destroy: true
+  validates :name, :price, :address, :age, :remarks, presence: true
 end
